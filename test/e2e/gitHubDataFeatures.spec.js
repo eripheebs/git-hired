a9bebc7845e11f920403f4c3e46c02e7b0964aa7
Tobenna <tobennandu@gmail.com>
2016-04-21 12:51:01 +0100

    Can see user's number of follower and number of repositories

--- test/e2e/gitHubDataFeatures.spec.js
+++ test/e2e/gitHubDataFeatures.spec.js
@@ -1,4 +1,6 @@
 describe('gitHubDataController',function () {
+  var user;
+  
   beforeEach(function () {
     browser.get('/');
   });
@@ -10,13 +12,26 @@ describe('gitHubDataController',function () {
 
   it('has user avatars', function () {
     var user = $$('#users p').first();
-    mypic = user.element(by.css("img[src*='https://avatars.githubusercontent.com/u/583231?v=3']"));
+    var mypic = user.element(by.css("img[src*='https://avatars.githubusercontent.com/u/583231?v=3']"));
     expect((mypic).isPresent()).toBe(true);
   });
 
-  it('displays the number of followers',function () {
-    var users = $$('#users p');
-    expect(users.first().getText()).toMatch('5');
-    expect(users.last().getText()).toMatch('1');
+  describe('user details',function () {
+
+    beforeEach(function () {
+      browser.get('/');
+      user = $$('#users p').first();
+    });
+
+    it('displays the number of followers',function () {
+      var followers = user.element(by.css(".followers"));
+      expect(followers.getText()).toMatch('5');
+    });
+
+    it('displays the number of repositories',function () {
+      var followers = user.element(by.css(".repos"));
+      expect(followers.getText()).toMatch('25');
+    });
   });
+
 });
